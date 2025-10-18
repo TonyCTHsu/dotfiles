fa2981a2d016c9aa9dd47c73afcf632ccd936cf2
Tony Hsu <tonyc.t.hsu@gmail.com>
2025-10-18 21:11:57 +0200

    Update color

--- zsh/configs/color.zsh
+++ zsh/configs/color.zsh
@@ -4,3 +4,6 @@ colors
 
 # enable colored output from ls, etc. on FreeBSD-based systems
 export CLICOLOR=1
+
+# Make directories bright cyan (much easier to read on dark backgrounds)
+export LSCOLORS=Gxfxcxdxbxegedabagacad
